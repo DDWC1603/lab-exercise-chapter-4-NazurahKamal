78197928a65eceb5a2b3395c7d545e0678d85e9d
NazurahKamal <33883971+NazurahKamal@users.noreply.github.com>
2018-01-15 09:54:12 +0800

    Add files via upload
    exercise 24

---
+++ chapter 4 looping.cpp
@@ -0,0 +1,26 @@
+//nazurah
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n = 3;
+	
+	while(n<4)
+	{
+		cout<<++n <<endl;
+	}
+	while (n>=0)
+	{
+		cout<<n*n <<endl;
+		--n;
+	}
+	cout<<n <<endl;
+	
+	while(n>=0)
+	{
+		cout<<(n/=2) <<endl;
+	}	
+	cout<<n <<endl;
+	
+	return 0;
+}
